8a74d8cd5adef1cd61ca49cfed1d3cdda754dcd4
Amanda Guerra <guerraamanda1@hotmail.com>
2022-08-26 10:51:48 -0300

    first commit

---
+++ src/server.js
@@ -0,0 +1,27 @@
+require("dotenv/config")
+require("express-async-errors")
+const express = require("express");
+const AppError = require("./utils/AppError")
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 3334;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+
+const routes = require("./routes");
+app.use(routes);
+
+app.use((error, req, res, next) => {
+  if(error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      status: "error",
+      message: error.message
+    })
+  }
+
+  return res.status(500).json({
+    status: "error",
+    message: "Internal server error"
+  })
+})
